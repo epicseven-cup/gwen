5ad76373119eb47802987747b3e8eca038ba9ee1
epicseven-cups <jackylin616@gmail.com>
2026-07-25 22:01:02 -0500

    updated

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import argparse
+import pymupdf
+
+
+BAD_BLOCK = (0,0,0,0)
 
 def main():
     parser = argparse.ArgumentParser(description="Gwen CLI")
@@ -13,8 +17,66 @@ def main():
     args = parser.parse_args()
 
 
-    with open("r", args.input_path) as f:
-        f.read()
+    pdf = pymupdf.open(args.input_path)
+
+
+
+    for p in range(len(pdf)):
+        page = pdf.load_page(p)
+        raw_dict = page.get_text("dict")
+
+        if not isinstance(raw_dict, dict):
+            raise ValueError("Invalid text dictionary format")
+
+        blocks = raw_dict.get("blocks", [])
+
+        page_blocks = blocks[:]
+
+
+        page_blocks.sort(key=lambda x: x["bbox"][1])
+
+        page_width = page.rect.width
+        for b in blocks:
+            t = b.get("type", -1)
+
+            x0, y0, x1, y1 = b.get("bbox", BAD_BLOCK)
+            if (x0, y0, x1, y1) == BAD_BLOCK:
+                raise ValueError("BAD BLOCK")
+
+
+            if (x1 - x0) > page_width * 0.7:
+                # Single column
+                middle_page_block.append(b)
+            elif (x1 - x0) > page_width * 0.5:
+                right_page_block.append(b)
+            else:
+                left_page_block.append(b)
+
+
+        left_page_block.sort(key=lambda xb: xb["bbox"][1])
+        right_page_block.sort(key=lambda xb: xb["bbox"][1])
+
+
+
+
+
+
+
+
+            match t:
+                case 0:
+                    pass
+                case 1:
+                    pass
+                case _:
+                    print("unknow block type passing")
+                    continue
+            pass
+
+
+
+
+
 
 
 
